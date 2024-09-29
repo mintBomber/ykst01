d8f6b0b3dfc69cfa843ec486c0b82f135fabdb12
mintBomber <131649216+mintBomber@users.noreply.github.com>
2024-09-29 21:28:28 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,51 @@
 import streamlit as st
 
-st.header('st.latex')
+# st.set_page_config(
+#     page_title="Words_app_top",
+#     page_icon="icon.png",
+#     layout="wide",
+#     # initial_sidebar_state="expanded",
+#     initial_sidebar_state="collapsed",
+#     menu_items={
+#         # 'Get Help': 'https://www.extremelycoolapp.com/help',
+#         # 'Report a bug': "https://www.extremelycoolapp.com/bug",
+#         'About': "# This is wordbook-app!"
+#     }
+# )
 
-st.latex(r'''
-     a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
-     \sum_{k=0}^{n-1} ar^k =
-     a \left(\frac{1-r^{n}}{1-r}\right)
-     ''')
+st.header('Welcome!')
+st.checkbox(label='Check')
+st.slider(label='Slider')
+st.selectbox(label='Choose Your Favourite Function', options=['','Create wordlist', 'Record', 'Setting'])
+
+# サイドバーにテーマの選択オプションを追加
+theme = st.sidebar.radio(
+    "テーマを選択してください",
+    options=["ライトモード", "ダークモード"]
+)
+
+# 選択されたテーマに基づいてスタイルを適用
+if theme == "ライトモード":
+    st.markdown(
+        """
+        <style>
+        body {
+            background-color: #FFFFFF;
+            color: #000000;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+else:
+    st.markdown(
+        """
+        <style>
+        body {
+            background-color: #2c3e50;
+            color: #ecf0f1;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
